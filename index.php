96589b3acc6a57178d3e67e9e2cea66f9352d439
lorenzo91347 <lorenzo.losito91@gmail.com>
2024-01-10 16:50:01 +0100

    feat: if/else

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 
+$input_email = $_POST ['email'];
+
 ?>
 
 
@@ -12,22 +14,27 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <title>PHP Iscrizione Newsletter
-    </title>
+    <title>PHP Iscrizione Newsletter</title>
 </head>
 <body>
 <div class="container">
     <h1 class="text-center">My Newsletter</h1>
+    <?php if(preg_match('(@|.)',$input_email) ):
+        echo '<div class="alert alert-primary" role="alert">
+        A simple primary alert—check it out!
+      </div>';
+    
+    else:
+        echo '<div class="alert alert-danger" role="alert">
+        A simple danger alert—check it out!
+      </div>'
+    endif;?>
 
-<form>
+<form action="index.php" method="POST" id="email" name="email">
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
