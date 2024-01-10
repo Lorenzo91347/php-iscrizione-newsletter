4690961d7c2d44634eeab5447dcb4519a78a19db
lorenzo91347 <lorenzo.losito91@gmail.com>
2024-01-10 18:01:25 +0100

    feat:the thankyou.php page now displays a thank you message

--- functions.php
+++ functions.php
@@ -1,14 +1,12 @@
 <?php 
 session_start();
 
-$_SESSION["newEmail"]=$input_email;
+
 $input_email = $_POST ['email'] ?? '';
 if($input_email) {
 
     if(str_contains($input_email, '@') && str_contains($input_email, '.')) {
-        echo '<div class="alert alert-primary" role="alert">
-        A simple primary alert—check it out!
-      </div>';
+        $_SESSION["newEmail"]=$input_email;
       header('Location: thankyou.php');
     
     } else {
--- thankyou.php
+++ thankyou.php
@@ -1,4 +1,19 @@
 <?php
 session_start();
-echo $_SESSION["newEmail"];
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Thank you</title>
+</head>
+<body>
+<div class="alert alert-primary text-center w-50 mx-auto" role="alert">
+    The email <?php echo $_SESSION["newEmail"];?>
+    went through successfully!
+</div>
+</body>
+</html>
